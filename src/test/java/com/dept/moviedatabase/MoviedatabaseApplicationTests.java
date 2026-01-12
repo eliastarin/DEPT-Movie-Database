40214aa179969be1b0a036a28b98efd22633ac35
Domnul Elias <89975213+eliastarin@users.noreply.github.com>
2026-01-12 19:16:59 +0200

    Project environment setup.

---
+++ src/test/java/com/dept/moviedatabase/MoviedatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dept.moviedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
